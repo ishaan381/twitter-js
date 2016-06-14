05d51b4b40ab1e769dd4f1b17eeefa5bf40658c2
ishaan381 <in2187@columbia.edu>
2016-06-14 14:55:38 -0400

    progress

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require('express');
+const chalk = require('chalk');
+
 var app = express();
 
 
 
+app.use(function(req, res, next) {
+  var str = req.method + ' ' + req.path + '\n';
+  process.stdout.write(chalk.red(str));
+  next();
+})
+
+app.use('/special/', function (req, res, next) {
+  res.send("You've reached the special area");
+});
+
 app.get('/', function(req, res) {
   res.send("server is listening")
+});
+
+app.get('/news', function(req, res) {
+  res.status(404).send("Page does not exist")
 });
 
 app.listen(3000, function() {

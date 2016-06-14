224051bb5d39db43a721e23ab1e86adb47418a12
ishaan381 <in2187@columbia.edu>
2016-06-14 15:32:45 -0400

    starting tweetbank

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 const chalk = require('chalk');
-
+const swig = require('swig');
 var app = express();
+
+var locals = {
+  title: 'An Example',
+  people: [{name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}]
+};
+
 
 
 
@@ -11,13 +17,22 @@ app.use(function(req, res, next) {
   next();
 })
 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname+'/views');
+
+app.get('/', function(req,res) {
+  res.render('index', locals)
+});
+
 app.use('/special/', function (req, res, next) {
   res.send("You've reached the special area");
 });
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
   res.send("server is listening")
-});
+});*/
+
 
 app.get('/news', function(req, res) {
   res.status(404).send("Page does not exist")
@@ -26,3 +41,13 @@ app.get('/news', function(req, res) {
 app.listen(3000, function() {
   console.log("listener");
 });
+
+
+swig.setDefaults({cache: false});
+
+
+
+
+/*swig.renderFile(__dirname + '/views/index.html', locals, function(err, output) {
+  console.log(output);
+});*/
